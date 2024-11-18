9dbf31c885025e300e38a95ffc7bf09e4497f373
Slavomir <Slavomir7115@gmail.com>
2024-11-18 19:03:33 +0100

    added new styles, fixed bug with value reset

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -15,13 +15,14 @@ const Todo = () => {
 
   return (
     <div className="h-screen w-screen bg-gradient-to-r from-gray-900 to-blue-950 flex items-center justify-center text-gray-100">
-      <div className="flex flex-row justify-center items-center gap-5 h-[600px] w-[1000px] border-gray-100 border-y-2 rounded-xl bg-gray-900 shadow-md p-20">
-        <div className="flex flex-col w-full h-full justify-start items-start gap-5">
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-5 h-[600px] w-[1000px] border-gray-100 border-y-2 rounded-xl bg-gray-900 shadow-md p-20">
+        <div className="flex flex-col w-full h-full justify-start items-center sm:items-start gap-5">
           <h1 className="text-3xl uppercase">Todo app</h1>
           <form className="flex flex-row gap-3" action="" onSubmit={addTodo}>
             <input
               className="border-gray-100 border-b-2 text-center  bg-transparent rounded-lg text-gray-100"
               required
+              value={todo}
               onChange={(e) => settodo(e.target.value)}
               type="text"
             />
@@ -34,13 +35,31 @@ const Todo = () => {
           </form>
         </div>
 
-        <div className="w-full h-full flex flex-col justify-start items-center gap-10">
+        <div className="w-full h-full flex flex-col justify-start items-center gap-10 overflow-y-auto">
           <h2 className="text-3xl">Your tasks</h2>
           {todos.map((item, index) => (
-            <div>
-              <label htmlFor="inputcheck">{item}</label>
-              <input key={index} type="checkbox" id="inputcheck" />
-              <button onClick={() => deleteTodo(index)}>Delete</button>
+            <div
+              key={index}
+              className="flex gap-10 justify-center items-center"
+            >
+              <input
+                type="checkbox"
+                id={`inputcheck-${index}`}
+                className="peer"
+              />
+              <label
+                htmlFor={`inputcheck-${index}`}
+                className="text-xl peer-checked:text-gray-400 cursor-crosshair"
+              >
+                {item}
+              </label>
+
+              <button
+                className="text-gray-100 bg-blue-950 py-2 px-3 uppercase rounded-lg"
+                onClick={() => deleteTodo(index)}
+              >
+                Delete
+              </button>
             </div>
           ))}
         </div>
